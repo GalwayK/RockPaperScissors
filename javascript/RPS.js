18d67c99838333914a13390124794badb509c6de
GalwayK <galwayk@sheridancollege.ca>
2022-06-06 19:10:01 -0400

    Add game rounds and victory logic.

--- javascript/RPS.js
+++ javascript/RPS.js
@@ -32,7 +32,7 @@ window.onload = () =>
         return playerHand;
     };
 
-    let playGame = () => 
+    let playRound = () => 
     {
         let playerHand = getPlayerHand();
         let computerHand = makeComputerHand();
@@ -43,14 +43,17 @@ window.onload = () =>
             if (computerHand === "rock")
             {
                 console.log("You lose!");
+                return false;
             }
             else if (computerHand === "paper")
             {
                 console.log("You win!");
+                return true;
             }
             else if (computerHand === "scissors")
             {
                 console.log("Draw!");
+                return;
             }
         }
         else if (playerHand === "paper")
@@ -58,14 +61,17 @@ window.onload = () =>
             if (computerHand === "rock")
             {
                 console.log("You win!");
+                return true;
             }
             else if (computerHand === "paper")
             {
                 console.log("Draw!");
+                return;
             }
             else if (computerHand === "scissors")
             {
                 console.log("You lose!");
+                return false;
             }
         }
         else if (playerHand === "rock")
@@ -73,17 +79,52 @@ window.onload = () =>
             if (computerHand === "rock")
             {
                 console.log("Draw!");
+                return;
             }
             else if (computerHand === "paper")
             {
                 console.log("You lose!");
+                return false;
             }
             else if (computerHand === "scissors");
             {
                 console.log("You win!");
+                return true;
             }
         }
 
+    };
+
+    let playGame = () =>
+    {
+        let playerScore = 0;
+        let computerScore = 0;
+
+        for (let r = 0; r < 5; r++)
+        {
+            let playerVictory = playRound();
+            if (playerVictory === true)
+            {
+                playerScore++;
+            }
+            else if (playerVictory === false)
+            {
+                computerScore++;
+            }
+        }
+
+        if (playerScore > computerScore)
+        {
+            console.log(`You have won the game ${playerScore} to ${computerScore}!`);
+        }
+        else if (computerScore > playerScore)
+        {
+            console.log(`The computer has won the game ${playerScore} to ${computerScore}!`);
+        }
+        else 
+        {
+            console.log(`The game was a draw ${playerScore} to ${computerScore}!`);
+        }
     };
 
     playGame();
